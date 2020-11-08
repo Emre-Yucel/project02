872911a79428c93dbb1523805c807d6309460a04
Emre Yucel <mr.emreyucel@gmail.com>
2020-11-08 20:08:00 +0300

    fort commit from local master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,14 +3,12 @@ package firstPackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-
 		System.out.println("first commit from local master branch");
 		System.out.println("second commit from local master");
 		System.out.println("third commit from local master");
 		System.out.println("first commit from remote master branch");
 		System.out.println("second commit from remote master branch");
 		System.out.println("Third commit from remote master branch");
-
+		System.out.println("Here is a merge conflict I create from local master");
 	}
-
 }
